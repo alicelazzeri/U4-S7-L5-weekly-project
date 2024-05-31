eee7a174d9d5dfdf9b3850c644643ca5f1e9ee53
Alice Lazzeri <alice.lazzeri@hotmail.it>
2024-05-31 10:14:19 +0200

    Initial commit

---
+++ src/main/java/it/epicode/U4_S7_L5_weekly_project/U4S7L5WeeklyProjectApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.U4_S7_L5_weekly_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U4S7L5WeeklyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U4S7L5WeeklyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/U4_S7_L5_weekly_project/U4S7L5WeeklyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.U4_S7_L5_weekly_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U4S7L5WeeklyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
